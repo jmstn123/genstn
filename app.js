3624095605d40c0e5643e7dbeda7de7ed038cccd
jamstain <james.stainer.20@ucl.ac.uk>
2023-01-25 13:10:40 +0000

    made a homepage

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ app.get("/token/:tokenId", async (req, res) => {
     );
   }
 
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+
   res.render("piece", {
     scriptName: `mySketch.js`,
   });
